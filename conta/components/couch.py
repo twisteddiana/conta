9ee3cd3051f3e576a7a04d1483f52a1c2dc1eff6
twisteddiana <twisteddiana@gmail.com>
2019-11-30 20:09:30 +0200

    basic auth

--- conta/components/couch.py
+++ conta/components/couch.py
@@ -4,6 +4,7 @@ import couch.couch
 from couch.couch import json_encode
 from tornado.escape import json_decode, url_escape
 import base64
+import os
 
 class MyAsyncCouch(couch.AsyncCouch):
 	@gen.coroutine
@@ -17,3 +18,10 @@ class MyAsyncCouch(couch.AsyncCouch):
 		url = '{0}/_explain'.format(self.db_name)
 		r = yield self._http_post(url, body=json_encode(body))
 		raise gen.Return(r)
+
+	def __init__(self, db_name='', **request_args):
+		couch_url = os.environ.get('COUCH_URL') or 'http://127.0.0.1:5984/'
+		request_args['auth_username'] = os.environ.get('COUCH_USER') or None
+		request_args['auth_password'] = os.environ.get('COUCH_PWD') or None
+
+		super(MyAsyncCouch, self).__init__(db_name, couch_url, None, **request_args)
