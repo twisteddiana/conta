4eeacacf0da8d4c89171d1ab3a6eab63c329de51
twisteddiana <twisteddiana@gmail.com>
2019-12-01 11:53:23 +0200

    authentication

--- conta/login/controller.py
+++ conta/login/controller.py
@@ -1,20 +1,19 @@
 import tornado.web
 from tornado import gen
-from components.couch import MyAsyncCouch
-import couch.couch
+from couch.couch import AsyncCouch
 
 
 class LoginHandler(tornado.web.RequestHandler):
     @gen.coroutine
     def get(self):
-        db = MyAsyncCouch()
+        db = AsyncCouch()
         response = yield db._http_get('/_session', self.request.headers)
         self.write(response)
         db.close()
 
     @gen.coroutine
     def post(self):
-        db = MyAsyncCouch()
+        db = AsyncCouch()
         print(self.request.body)
         try:
             response = yield db._http_post('/_session', self.request.body, headers=self.request.headers)
